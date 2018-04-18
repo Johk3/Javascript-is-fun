a0350232095b1943efb5422c9a2c34fd184e15b2
Johk3 <johkmr@gmail.com>
2018-04-18 21:51:22 +0300

    Added my startup website

--- funnysite/main.js
+++ funnysite/main.js
@@ -1,11 +1,5 @@
 function setup(){
-	var width = window.innerWidth
-	|| document.documentElement.clientWidth
-	|| document.body.clientWidth;
-    var height = window.innerHeight
-	|| document.documentElement.clientHeight
-	|| document.body.clientHeight;
-	createCanvas(width, height)
+	createCanvas(windowWidth, windowHeight)
 }
 function draw(){
 	random = Math.floor((Math.random() * 300) + 0);
